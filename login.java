775bac0e44ddb0028b55d03dc1d9961137779bc0
Tegar Mandiri <TegarMandiri@users.noreply.github.com>
2019-06-11 18:24:26 +0700

    Update login.java
    - Renamed Variable

--- login.java
+++ login.java
@@ -1,5 +1,7 @@
 //Finished, will added more features++
-//Ver. 1.05
+//Ver. 1.50
+//Credit Tegar Mandiri 2019
+//Contact email: tegarmndr@gmail.com
 
 import java.util.Scanner;
 
@@ -7,57 +9,65 @@ public class login{
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
 
-//Credit Tegar Mandiri 2019
-//Contact email: tegarmndr@gmail.com
-
 		System.out.println(" ");
 		System.out.println(" ");
 		System.out.println("//Copyright Tegar Mandiri 2019");
-		System.out.println("-Ver. 1.05");
+		System.out.println("-Ver. 1.50");
 		System.out.println(" ");
 		System.out.println("   ------- Login Simulation -------    ");
 		System.out.println(" ");
 		System.out.println("--------------- Sign Up ---------------");
 		System.out.println(" ");
 		System.out.print("Username 	: ");
-		String unm = scan.nextLine();
+		String usernameSignUp = scan.nextLine();
 
 		System.out.print("Password 	: ");
-		String pwd = scan.nextLine();
+		String passwordSignUp = scan.nextLine();
 
 		System.out.print("Repeat Password : ");
-		String rpwd = scan.nextLine();
+		String repeatPassword = scan.nextLine();
 
-		if (pwd.equals(rpwd)){
+		if (passwordSignUp.equals(repeatPassword)){
+
 			System.out.println(" ");
 			System.out.println(" ");
 			System.out.println("--------------- Sign In ---------------");
 			System.out.println(" ");
 			System.out.print("Username 	: ");
-			String unm2 = scan.nextLine();
+			String usernameSignIn = scan.nextLine();
 
-			if (unm.equals(unm2)){
+			if (usernameSignUp.equals(usernameSignIn)){
+
 				System.out.print("Password 	: ");
-				String pwd2 = scan.nextLine();
+				String passwordSignIn = scan.nextLine();
 
-				if (pwd.equals(pwd2)){
+				if (passwordSignUp.equals(passwordSignIn)){
+
 					System.out.println("---------------");
 					System.out.println("Login Success");
-				}else{
+
+				} else {
+
 					System.out.println("---------------");
 					System.out.println("Login Failed");
+
 				}
 
-			}else{
+			} else {
+
 				System.out.println("---------------");
 				System.out.println("Wrong Username");
+
 			}
 
-		}else{
+		} else {
+
 			System.out.println("---------------");
+		
 			System.out.println("Wrong Password");
 		}
 
 		System.out.println(" ");
+
 	}
 }
